c37ad83f6abbf5cc0da77793ba319d26e2109e25
esphynol <70239047+esphynol@users.noreply.github.com>
2020-08-28 01:11:18 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-jQuery(function ($) {
+//jQuery(function ($) {
 
-    'use strict';
+//    'use strict';
 
     // --------------------------------------------------------------------
     // PreLoader
     // --------------------------------------------------------------------
 
-    (function () {
-        $('#preloader').delay(200).fadeOut('slow');
-    }());
+//    (function () {
+//        $('#preloader').delay(200).fadeOut('slow');
+//    }());
 
 
 
@@ -16,6 +16,66 @@ jQuery(function ($) {
     // Sticky Sidebar
     // --------------------------------------------------------------------
 
-    $('.left-col-block, .right-col-block').theiaStickySidebar();
+//    $('.left-col-block, .right-col-block').theiaStickySidebar();
 
-}); // JQuery end
+//}); // JQuery end
+
+
+
+
+<script language=javascript>
+//change 1 to another integer to alter the scroll speed. Greater is faster
+var speed=1
+var downspeed= 2
+var stopspeed= 0
+var currentpos=0
+var alt = 0
+var curpos1 = 100
+var curpos2 = 1
+function initialize(){
+ startit()
+}
+function goup(e) {
+ speed = stopspeed
+}
+function godown(e) {
+ speed = downspeed
+}
+function scrollwindow(){
+ if (document.all) {
+  temp=document.body.scrollDown;
+ }
+ else {
+  temp=window.pageYOffset;
+ }
+ if (alt==0) {
+  alt=1;
+  curpos2 = temp;
+ }
+ else {
+  alt=0;
+  curpos1 = temp;
+ }
+ if ((curpos1==curpos2) && (speed > 0)) {
+  currentpos = 0;
+  curpos1 = 100;
+ }
+ else {
+  if (document.all) {
+   currentpos=document.body.scrollDown+speed;
+  }
+  else {
+	currentpos=window.pageYOffset+speed;
+  }
+ }
+ if (speed > 0) { window.scrollTo(0,currentpos); }
+ setTimeout("scrollwindow()", 20);
+}
+function startit(){
+ setTimeout("scrollwindow()", 20)
+}
+window.onload=initialize
+document.onmousedown = goup;
+document.ondblclick = godown;
+
+</script> 
